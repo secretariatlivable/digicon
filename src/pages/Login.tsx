6a7de3c3bd3bd1af8a4bdb0424934c7cbac9c4cd
secretariatlivable <secretariat.livable@gmail.com>
2026-09-03 16:30:50 +0000

    Fix all experted expressions

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,10 @@
 import { AuthPage } from "@/pages/AuthPage";
 
 export default function Login() {
+<<<<<<< HEAD
   return <AuthPage defaultMode="signin" />;
 }
+=======
+  return <AuthPage mode="login" />;
+ };
+>>>>>>> 54b2bd8 (Fix all experted expressions)
