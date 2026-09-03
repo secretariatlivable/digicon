efa6745e5e7428778a987f49f82f4f656e2277de
secretariatlivable <secretariat.livable@gmail.com>
2026-09-04 02:31:32 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Landing from "@/pages/Landing";
 import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
 import Onboarding from "@/pages/Onboarding";
-import Dashboard from "@/pages/DashboardPage";
+import Dashboard from "@/pages/Dashboard";
 import MyCards from "@/pages/MyCards";
 import CardBuilder from "@/pages/CardBuilder";
 import Share from "@/pages/Share";
